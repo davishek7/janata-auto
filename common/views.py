0de34dea7c03ac124699b33f5a572a993f04eec9
Avishek Das <davishek7@gmail.com>
2022-08-08 22:58:21 +0530

    started working on search

--- common/views.py
+++ common/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.db.models import Q
 from .forms import SearchForm
 from django.http import JsonResponse
-from asset.models import Battery, DistilledWater, EngineOil
+from asset.models import Battery, DistilledWater, EngineOil, Inverter
+from sale.models import BatterySale, EngineOilSale, DistilledWaterSale, ScrapBatterySale, InverterSale
+from service.models import BatteryWarrantyClaim
 from product.models import Product
 from vendor.models import Vendor
 
@@ -14,6 +16,4 @@ def search(request):
     if search_form.is_valid():
         search_term = search_form.cleaned_data.get('q')
 
-        products = Product.objects.filter(Q(name__icontains = search_term)|Q(size__size__icontains=search_term)).values()
-        print(products)
-        return JsonResponse(list(products), safe=False)
+        products = Product.objects.filter(Q(name__icontains = search_term)|Q(size__size__icontains=search_term)).all()
